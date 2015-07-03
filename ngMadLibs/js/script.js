7e29f5fe49905f54b2b68547838e136fe888f2de
avireni <beena@avireni.com>
2015-07-03 11:57:05 -0400

    Initial commit MadLibs

---
+++ ngMadLibs/js/script.js
@@ -0,0 +1,12 @@
+angular.module("myApp", [])
+.controller('madlibsCtrl', function($scope){
+    $scope.name= "Name";
+    $scope.jobtitle= "Job title";
+    $scope.tedioustask ="Tedious task";
+    $scope.dirtytask= "Dirty task";
+    $scope.celebrity= "Celebrity";
+    $scope.useless= "Useless";
+    $scope.obcelebrity= "Ob celebrity";
+    $scope.number= "Number";
+    $scope.adjective= "Adjective";
+});
