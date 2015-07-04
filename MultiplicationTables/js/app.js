41f7cd2498e0befa36098093a299ac0a685f6dc8
avireni <beena@avireni.com>
2015-07-04 18:30:28 -0400

    dynamic limit

--- MultiplicationTables/js/app.js
+++ MultiplicationTables/js/app.js
@@ -1,4 +1,15 @@
 angular.module("myApp", [])
-.controller("multiplyCtrl", function($scope) {
-    $scope.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-});
+    .controller("multiplyCtrl", function ($scope, $attrs) { // $attrs - Current attributes object for the element
+        //$scope.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        $scope.numberLimit = $attrs.initialNumberLimit || 10;
+        $scope.numbers = function () {
+            var numbers = [];
+            for (var i = 0; i < $scope.numberLimit; i++) {
+                numbers[i] = i + 1;
+            }
+            return numbers;
+        };
+        $scope.multiply = function (a, b) {
+            return a * b;
+        };
+    });
