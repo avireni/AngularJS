5258f4b9343da7fab0f8caa853abc26bf7a314c2
avireni <beena@avireni.com>
2015-07-06 14:39:12 -0400

    validation and views

--- ngMadLibs/js/script.js
+++ ngMadLibs/js/script.js
@@ -1,12 +1,40 @@
 angular.module("myApp", [])
-.controller('madlibsCtrl', function($scope){
-    $scope.name= "Name";
-    $scope.jobtitle= "Job title";
-    $scope.tedioustask ="Tedious task";
-    $scope.dirtytask= "Dirty task";
-    $scope.celebrity= "Celebrity";
-    $scope.useless= "Useless";
-    $scope.obcelebrity= "Obnoxious celebrity";
-    $scope.number= "Number";
-    $scope.adjective= "Adjective";
-});
+    .controller('madlibsCtrl', ['$scope', function ($scope) {
+        $scope.submitted = false;
+        //    $scope.name= "Name";
+        //    $scope.jobtitle= "Job title";
+        //    $scope.tedioustask ="Tedious task";
+        //    $scope.dirtytask= "Dirty task";
+        //    $scope.celebrity= "Celebrity";
+        //    $scope.useless= "Useless";
+        //    $scope.obcelebrity= "Obnoxious celebrity";
+        //    $scope.number= 10;
+        //    $scope.adjective= "Adjective";
+        $scope.data = {
+            name: null,
+            jobtitle: null,
+            tedioustask: null,
+            dirtytask: null,
+            celebrity: null,
+            useless: null,
+            obcelebrity: null,
+            number: null,
+            adjective: null
+
+
+        };
+        $scope.submit = function () {
+            if ($scope.myForm.$valid) {
+                console.log('form is valid');
+                $scope.submitted = true;
+            } else {
+                $scope.submitted = false;
+                console.log('form is invalid');
+            }
+        }
+        $scope.startOver = function () {
+            $scope.submitted = false;
+            $scope.data = {};
+
+        }
+}]);
