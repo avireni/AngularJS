f8787c29fb8d2cc9728b9f9657a70b16e208005a
avireni <beena@avireni.com>
2015-07-12 14:26:06 -0400

    code refactor

--- InstagramSearcher/js/app.js
+++ InstagramSearcher/js/app.js
@@ -3,32 +3,49 @@ angular.module("instaSearch", [])
 		$scope.searchQuery = "";
 		$scope.message = "";
 		$scope.submitForm = false;
+		$scope.hasError = false;
 		$scope.results = {};
+		var config;
 
 		function wait() {
 			var defer = $q.defer();
 			setTimeout(function () {
 				defer.resolve();
 			}, 2000);
+
 			return defer.promise;
 		}
-		$scope.submit = function () {
-			var tag = $scope.searchQuery;
-			if ($scope.myForm.$valid) {
-				$scope.submitForm = true;
-				var config = {
-					'params': {
-						client_id: 'df1be7028ee843dcb68036adc5bfc557',
-						callback: 'JSON_CALLBACK'
-					}
-				}
-				$http.jsonp("https://api.instagram.com/v1/tags/" + tag + "/media/recent", config)
-					.success(function (result) {
-						$scope.results = result.data;
-						$scope.resultCount = $scope.results.length || 0;
-						console.log($scope.results);
-					});
 
+		function submitReq() {
+			$scope.submitForm = true;
+			config = {
+				'params': {
+					client_id: 'df1be7028ee843dcb68036adc5bfc557',
+					callback: 'JSON_CALLBACK'
+				}
 			}
 		}
-}]);
+
+		function getResults() {
+			var tag = $scope.searchQuery;
+			$http.jsonp("https://api.instagram.com/v1/tags/" + tag + "/media/recent", config)
+				.success(function (result) {
+					$scope.results = result.data;
+					$scope.resultCount = $scope.results.length || 0;
+					console.log($scope.results);
+					$scope.message = "We found " + $scope.resultCount + " results for " + tag;
+				})
+				.error(function (result) {
+					$scope.hasError = true;
+					$scope.message = "Instagram Server fails to respond";
+				});
+		}
+
+		$scope.submit = function () {
+			if ($scope.myForm.$valid) {
+				$scope.message = "Searching results for " + $scope.searchQuery;
+				submitReq();
+				return wait().then(getResults);
+			}
+		}
+	}]);
