2d5351ebb2075059b5709c50505a7a8ba5b460bd
avireni <beena@avireni.com>
2015-08-01 09:42:18 -0400

    Added Routes for Waitstaff calculator

--- WaitstaffRefactor/js/app.js
+++ WaitstaffRefactor/js/app.js
@@ -1,4 +1,4 @@
-angular.module("myApp", ['ngRoute'])
+angular.module("myApp", ['ngRoute', 'ngAnimate'])
 	.controller("MainCtrl", ['$rootScope', function ($rootScope) {
 		$rootScope.data = {};
 		$rootScope.total = {};
@@ -6,7 +6,19 @@ angular.module("myApp", ['ngRoute'])
 
 
 
-}])
+}]).run(function ($rootScope, $location, $timeout) {
+		$rootScope.$on('$routeChangeError', function () {
+			$location.path("/error");
+		});
+		$rootScope.$on('$routeChangeStart', function () {
+			$rootScope.isLoading = true;
+		});
+		$rootScope.$on('$routeChangeSuccess', function () {
+			$timeout(function () {
+				$rootScope.isLoading = false;
+			}, 1000);
+		});
+	})
 	.config(function ($routeProvider) {
 		$routeProvider.when('/', {
 			templateUrl: './home.tmpl.html',
